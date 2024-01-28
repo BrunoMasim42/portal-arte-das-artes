f66c8551253a512d04aa370f12ccb889948b19cb
Bruno Masim <vinimasim.simoes@gmail.com>
2024-01-28 01:24:49 -0300

    primeiro commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+fetch('menu.html')
+.then(response => response.text())
+.then(data => {
+    document.getElementById('menu-container').innerHTML = data;
+})
+.catch(error => {
+    console.error('Ocorreu um erro ao buscar o menu');
+})
